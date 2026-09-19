16908a1724c5777efacd615882cc0f652bf116fb
Bkzazzau1 <y.i.bashir@gmail.com>
2026-09-19 23:01:04 +0100

    test(finance): expose reconciliation review form vertically

--- test/finance_reconciliation_feature_test.dart
+++ test/finance_reconciliation_feature_test.dart
@@ -97,7 +97,7 @@ void main() {
   });
 
   testWidgets('manual review proposal does not turn ambiguous transfer into matched payment', (tester) async {
-    tester.view.physicalSize = const Size(1800, 1600);
+    tester.view.physicalSize = const Size(1800, 2800);
     tester.view.devicePixelRatio = 1;
     addTearDown(tester.view.resetPhysicalSize);
     addTearDown(tester.view.resetDevicePixelRatio);
