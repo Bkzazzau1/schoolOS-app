0b585e0dcfc158d1d39b068e0d137fe9b1d9d474
Bkzazzau1 <y.i.bashir@gmail.com>
2026-09-19 20:08:02 +0100

    feat(finance): add Finance Officer to native membership routing

--- lib/features/authentication/presentation/login_page.dart
+++ lib/features/authentication/presentation/login_page.dart
@@ -5,6 +5,7 @@ import '../../../shared/layout/app_breakpoints.dart';
 import '../../../shared/models/school_membership.dart';
 import '../../administrator/presentation/administrator_workspace_page.dart';
 import '../../dashboard/presentation/dashboard_page.dart';
+import '../../finance_office/presentation/finance_office_workspace_page.dart';
 import '../../principal/presentation/principal_workspace_page.dart';
 import '../../proprietor/presentation/proprietor_workspace_page.dart';
 import '../../school_switcher/presentation/school_selection_page.dart';
@@ -39,6 +40,12 @@ class _LoginPageState extends State<LoginPage> {
       schoolId: 'school-brightgate',
       schoolName: 'BrightGate Academy',
       role: SchoolRole.administrator,
+    ),
+    SchoolMembership(
+      id: 'membership-finance-001',
+      schoolId: 'school-brightgate',
+      schoolName: 'BrightGate Academy',
+      role: SchoolRole.accountant,
     ),
     SchoolMembership(
       id: 'membership-principal-001',
@@ -174,6 +181,13 @@ class _LoginPageState extends State<LoginPage> {
       );
     } else if (membership.role == SchoolRole.administrator) {
       page = AdministratorWorkspacePage(
+        membership: membership,
+        localDatabase: widget.services.localDatabase,
+        schoolSession: widget.services.schoolSession,
+        schoolAppearance: widget.services.schoolAppearance,
+      );
+    } else if (membership.role == SchoolRole.accountant) {
+      page = FinanceOfficeWorkspacePage(
         membership: membership,
         localDatabase: widget.services.localDatabase,
         schoolSession: widget.services.schoolSession,
