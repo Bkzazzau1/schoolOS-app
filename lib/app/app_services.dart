a3d0c00674984d92a17a3f35e9197f1c174ec21c
Bkzazzau1 <y.i.bashir@gmail.com>
2026-09-19 10:30:52 +0100

    Bootstrap school appearance service

--- lib/app/app_services.dart
+++ lib/app/app_services.dart
@@ -1,3 +1,4 @@
+import '../core/appearance/school_appearance_controller.dart';
 import '../core/database/local_database.dart';
 import '../core/security/payload_cipher.dart';
 import '../core/tenancy/school_session_controller.dart';
@@ -7,10 +8,12 @@ class AppServices {
   AppServices._({
     required this.localDatabase,
     required this.schoolSession,
+    required this.schoolAppearance,
   });
 
   final LocalDatabase localDatabase;
   final SchoolSessionController schoolSession;
+  final SchoolAppearanceController schoolAppearance;
 
   static Future<AppServices> bootstrap() async {
     final cipher = PayloadCipher();
@@ -22,9 +25,16 @@ class AppServices {
     );
     await schoolSession.restore();
 
+    final schoolAppearance = SchoolAppearanceController(
+      localDatabase: localDatabase,
+      schoolSession: schoolSession,
+    );
+    await schoolAppearance.initialize();
+
     return AppServices._(
       localDatabase: localDatabase,
       schoolSession: schoolSession,
+      schoolAppearance: schoolAppearance,
     );
   }
 }
