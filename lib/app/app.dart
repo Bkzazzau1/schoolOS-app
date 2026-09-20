963fa226315216306f435b32998392fc33299194
Bkzazzau1 <y.i.bashir@gmail.com>
2026-09-20 10:35:15 +0100

    feat(parent): route restored parent sessions to family portal

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -4,6 +4,7 @@ import '../features/administrator/presentation/administrator_workspace_page.dart
 import '../features/authentication/presentation/login_page.dart';
 import '../features/dashboard/presentation/dashboard_page.dart';
 import '../features/finance_office/presentation/finance_office_workspace_page.dart';
+import '../features/parent/presentation/parent_workspace_page.dart';
 import '../features/principal/presentation/principal_workspace_page.dart';
 import '../features/proprietor/presentation/proprietor_workspace_page.dart';
 import '../features/teacher/presentation/teacher_workspace_page.dart';
@@ -72,6 +73,13 @@ class SchoolOsApp extends StatelessWidget {
           );
         } else if (restoredMembership.role == SchoolRole.teacher) {
           home = TeacherWorkspacePage(
+            membership: restoredMembership,
+            localDatabase: services.localDatabase,
+            schoolSession: services.schoolSession,
+            schoolAppearance: services.schoolAppearance,
+          );
+        } else if (restoredMembership.role == SchoolRole.parent) {
+          home = ParentWorkspacePage(
             membership: restoredMembership,
             localDatabase: services.localDatabase,
             schoolSession: services.schoolSession,
